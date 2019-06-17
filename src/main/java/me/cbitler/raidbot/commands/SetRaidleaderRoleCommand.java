e984179c37dc2260b51bc0df16ce7fc24c0e6e29
Stefan Lindstrom <stefan.lindstrom@xlent.se>
2019-06-17 14:35:36 +0200

    Added a check for Permissions.MANAGE_SERVER in SetRaidleaderRoleCommand

--- src/main/java/me/cbitler/raidbot/commands/SetRaidleaderRoleCommand.java
+++ src/main/java/me/cbitler/raidbot/commands/SetRaidleaderRoleCommand.java
@@ -4,25 +4,29 @@ import me.cbitler.raidbot.RaidBot;
 import me.cbitler.raidbot.raids.Raid;
 import me.cbitler.raidbot.raids.RaidManager;
 import me.cbitler.raidbot.raids.RaidUser;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 
 public class SetRaidleaderRoleCommand implements Command {
     @Override
     public void handleCommand(String command, String[] args, TextChannel channel, User author) {
-        if (args.length >= 1) {
-            try {;
-                String raidLeaderRole = args[0];
-                RaidBot.getInstance().setRaidLeaderRole(channel.getGuild().getId(), raidLeaderRole);
+        if (channel.getGuild().getMember(author).getPermissions().contains(Permission.MANAGE_SERVER)) {
+            if (args.length >= 1) {
+                try {
+                    ;
+                    String raidLeaderRole = args[0];
+                    RaidBot.getInstance().setRaidLeaderRole(channel.getGuild().getId(), raidLeaderRole);
+                    author.openPrivateChannel().queue(privateChannel -> privateChannel
+                            .sendMessage("Raid leader role updated to: " + raidLeaderRole).queue());
+                } catch (Exception exc) {
+                    author.openPrivateChannel().queue(privateChannel -> privateChannel
+                            .sendMessage("Make sure that the bot has the 'Manage messages' permission").queue());
+                }
+            } else {
                 author.openPrivateChannel().queue(privateChannel -> privateChannel
-                        .sendMessage("Raid leader role updated to: " + raidLeaderRole).queue());
-            } catch (Exception exc) {
-                author.openPrivateChannel().queue(privateChannel -> privateChannel
-                        .sendMessage("Make sure that the bot has the 'Manage messages' permission").queue());
+                        .sendMessage("Format for !removeFromRaid: !removeFromRaid [raid id] [name]").queue());
             }
-        } else {
-            author.openPrivateChannel().queue(privateChannel -> privateChannel
-                    .sendMessage("Format for !removeFromRaid: !removeFromRaid [raid id] [name]").queue());
         }
     }
 }
